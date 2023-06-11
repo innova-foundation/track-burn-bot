81092fd3cfb589a5d66b135af3374349425402cf
0xCiruitBreaker <sarris88p@gmail.com>
2023-06-11 10:20:29 -0400

    add intents

--- burnbot.py
+++ burnbot.py
@@ -1,7 +1,8 @@
 from discord.ext import commands, tasks
 import discord
 
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # initialize total burned coins
 global_total_burned_coins = 0
