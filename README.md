783c5dd142fc5015c1c11863004cc43c114d4312
0xCiruitBreaker <sarris88p@gmail.com>
2023-06-11 09:58:49 -0400

    add requirements.txt, add burned coin in current tx, and more detailed ReadMe for usage

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # track-burn-bot
+
+This bot is designed to detect burn transactions in a blockchain and post information about those transactions to a Discord channel.
+
+## Prerequisites
+
+You need Python 3.6 or later to run this bot. You can verify your Python version by running python --version in your command prompt.
+
+You also need the following Python packages which can be installed via pip:
+
+    discord.py
+    aiohttp
+
+Install them using this command:
+
+```pip install -r requirements.txt```
+
+## Configuration
+
+Replace `'your-bot-token'` in `bot.run('your-bot-token')` with the token of your Discord bot. The token can be obtained from the Discord Developer Portal.
+
+Ensure you replace the 15 second interval to your desired interval on line 11
+
+This bot uses getinfo to retrieve blockchain info, if your blockchain uses a newer BTC Core and require the `getblockchaininfo` command be sure to simply replace `getinfo` with `getblockchaininfo` (Or which ever command grabs the blockchain info with the `blocks` data)
+
+Also, ensure that you change the channel name from "burn-transactins" to the channel you want the bot to post to
+
+## Running the Bot
+
+To start the bot, navigate to the directory containing bot.py in your command prompt and run:
+
+```python burnbot.py```
+
+The bot will start and attempt to connect to Discord. Once connected, it will print a message in the command prompt containing its username.
+
+## Usage
+
+Once the bot is running, it will automatically start checking for burn transactions in the blockchain every 15 seconds (Or whatever you changed it to). When it detects a burn transaction, it will send a message to the discord channel in your Discord server that you designated, with information about the block number, block hash, transaction ID, burned coins in current transaction ID, and the total burned coins.
--- burnbot.py
+++ burnbot.py
@@ -3,6 +3,9 @@ import discord
 
 bot = commands.Bot(command_prefix="!")
 
+# initialize total burned coins
+global_total_burned_coins = 0
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
@@ -10,6 +13,7 @@ async def on_ready():
 
 @tasks.loop(seconds=15)  # desired interval
 async def burn_check():
+    global global_total_burned_coins
     try:
         # fetch the latest block
         latest_block = request('http://localhost:14531', 'getinfo')['blocks']
@@ -20,20 +24,22 @@ async def burn_check():
         for txid in block['tx']:
             tx = request('http://localhost:14531', 'getrawtransaction', txid, 1)
             # initialize total burned coins for this transaction
-            total_burned_coins = 0
+            total_burned_coins_this_tx = 0
             # check each output script for the OP_RETURN opcode
             for vout in tx['vout']:
                 if vout['scriptPubKey']['asm'].startswith('OP_RETURN'):
                     # add the value of this output to the total burned coins
-                    total_burned_coins += vout['value']
-            if total_burned_coins > 0:
+                    total_burned_coins_this_tx += vout['value']
+            if total_burned_coins_this_tx > 0:
+                global_total_burned_coins += total_burned_coins_this_tx
                 # send a message if there were any burned coins in this transaction
                 channel = discord.utils.get(bot.get_all_channels(), name='burn-transactions')
                 await channel.send(f'Burn transaction detected!\n'
                                    f'Block number: {latest_block}\n'
                                    f'Block hash: {block_hash}\n'
                                    f'Transaction ID: {txid}\n'
-                                   f'Total burned coins: {total_burned_coins}')
+                                   f'Burned coins in this TX: {total_burned_coins_this_tx}\n'
+                                   f'Total burned coins: {global_total_burned_coins}')
 
     except Exception as e:
         print(f'Error: {e}')
