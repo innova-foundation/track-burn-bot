842a940b1bb39b2ea06c20cbaaf860acc464c1c2
0xCiruitBreaker <sarris88p@gmail.com>
2023-06-11 10:42:46 -0400

    fix for aiohttp from request

--- burnbot.py
+++ burnbot.py
@@ -1,5 +1,7 @@
 from discord.ext import commands, tasks
 import discord
+import aiohttp
+import json
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -15,35 +17,43 @@ async def on_ready():
 @tasks.loop(seconds=15)  # desired interval
 async def burn_check():
     global global_total_burned_coins
-    try:
-        # fetch the latest block
-        latest_block = request('http://localhost:14531', 'getinfo')['blocks']
-        block_hash = request('http://localhost:14531', 'getblockhash', latest_block)
-        block = request('http://localhost:14531', 'getblock', block_hash)
 
-        # iterate through each transaction in the block
-        for txid in block['tx']:
-            tx = request('http://localhost:14531', 'getrawtransaction', txid, 1)
-            # initialize total burned coins for this transaction
-            total_burned_coins_this_tx = 0
-            # check each output script for the OP_RETURN opcode
-            for vout in tx['vout']:
-                if vout['scriptPubKey']['asm'].startswith('OP_RETURN'):
-                    # add the value of this output to the total burned coins
-                    total_burned_coins_this_tx += vout['value']
-            if total_burned_coins_this_tx > 0:
-                global_total_burned_coins += total_burned_coins_this_tx
-                # send a message if there were any burned coins in this transaction
-                channel = discord.utils.get(bot.get_all_channels(), name='burn-transactions')
-                await channel.send(f'Burn transaction detected!\n'
-                                   f'Block number: {latest_block}\n'
-                                   f'Block hash: {block_hash}\n'
-                                   f'Transaction ID: {txid}\n'
-                                   f'Burned coins in this TX: {total_burned_coins_this_tx}\n'
-                                   f'Total burned coins: {global_total_burned_coins}')
+    async with aiohttp.ClientSession() as session:
+        try:
+            # fetch the latest block
+            async with session.post('http://localhost:14531', json={'method': 'getinfo'}) as response:
+                latest_block = (await response.json())['result']['blocks']
+            
+            async with session.post('http://localhost:14531', json={'method': 'getblockhash', 'params': [latest_block]}) as response:
+                block_hash = (await response.json())['result']
+            
+            async with session.post('http://localhost:14531', json={'method': 'getblock', 'params': [block_hash]}) as response:
+                block = (await response.json())['result']
 
-    except Exception as e:
-        print(f'Error: {e}')
+            # iterate through each transaction in the block
+            for txid in block['tx']:
+                async with session.post('http://localhost:14531', json={'method': 'getrawtransaction', 'params': [txid, 1]}) as response:
+                    tx = (await response.json())['result']
+                # initialize total burned coins for this transaction
+                total_burned_coins_this_tx = 0
+                # check each output script for the OP_RETURN opcode
+                for vout in tx['vout']:
+                    if vout['scriptPubKey']['asm'].startswith('OP_RETURN'):
+                        # add the value of this output to the total burned coins
+                        total_burned_coins_this_tx += vout['value']
+                if total_burned_coins_this_tx > 0:
+                    global_total_burned_coins += total_burned_coins_this_tx
+                    # send a message if there were any burned coins in this transaction
+                    channel = discord.utils.get(bot.get_all_channels(), name='burn-transactions')
+                    await channel.send(f'Burn transaction detected!\n'
+                                        f'Block number: {latest_block}\n'
+                                        f'Block hash: {block_hash}\n'
+                                        f'Transaction ID: {txid}\n'
+                                        f'Burned coins in this TX: {total_burned_coins_this_tx}\n'
+                                        f'Total burned coins: {global_total_burned_coins}')
+
+        except Exception as e:
+            print(f'Error: {e}')
 
 # replace with your actual bot token
 bot.run('your-bot-token')
