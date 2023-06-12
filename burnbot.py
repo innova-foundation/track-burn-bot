87d567fce908967f60a7f422b54787d1c9c1e272
0xCiruitBreaker <sarris88p@gmail.com>
2023-06-11 21:57:05 -0400

    move open/close

--- burnbot.py
+++ burnbot.py
@@ -44,19 +44,22 @@ def get_total_burned_coins_from_db():
         return 0.0
     return result[0]
 
-def update_last_processed_block_in_db(block_num):
-    conn = sqlite3.connect('burnbot.db')
-    c = conn.cursor()
-    c.execute("UPDATE burnbot SET last_processed_block = ?", (block_num,))
-    conn.commit()
-    conn.close()
+def update_last_processed_block_in_db(c, block_num):
+    c.execute("SELECT count(*) FROM burnbot")
+    result = c.fetchone()
+    if result[0] == 0:
+        c.execute("INSERT INTO burnbot (last_processed_block, total_burned_coins) VALUES (?, 0)", (block_num,))
+    else:
+        c.execute("UPDATE burnbot SET last_processed_block = ?", (block_num,))
 
-def update_total_burned_coins_in_db(total_burned):
-    conn = sqlite3.connect('burnbot.db')
-    c = conn.cursor()
-    c.execute("UPDATE burnbot SET total_burned_coins = ?", (total_burned,))
-    conn.commit()
-    conn.close()
+def update_total_burned_coins_in_db(c, total_burned):
+    c.execute("SELECT count(*) FROM burnbot")
+    result = c.fetchone()
+    if result[0] == 0:
+        c.execute("INSERT INTO burnbot (last_processed_block, total_burned_coins) VALUES (-1, ?)", (total_burned,))
+    else:
+        c.execute("UPDATE burnbot SET total_burned_coins = ?", (total_burned,))
+
 
 # read last processed block and total burned coins from DB
 last_processed_block = get_last_processed_block_from_db()
@@ -71,6 +74,10 @@ async def on_ready():
 @tasks.loop(seconds=15)  # desired interval
 async def burn_check():
     global global_total_burned_coins, last_processed_block
+
+    # Open the database connection here
+    conn = sqlite3.connect('burnbot.db')
+    c = conn.cursor()
 
     async with aiohttp.ClientSession(auth=auth) as session:
         try:
@@ -118,7 +125,8 @@ async def burn_check():
 
             if total_burned_coins_this_block > 0:
                 global_total_burned_coins += total_burned_coins_this_block
-                update_total_burned_coins_in_db(global_total_burned_coins)
+                update_total_burned_coins_in_db(c, global_total_burned_coins) # pass the cursor to the update function
+                conn.commit()
                 print(f'Detected burn transaction {burn_txid_this_block} in block {latest_block} with {total_burned_coins_this_block} burned coins')  # Debug line
                 # send a message if there were any burned coins in this transaction
                 channel = bot.get_channel(CHANNEL_ID) # change with desired CHANNEL_ID
@@ -135,7 +143,8 @@ async def burn_check():
 
             # update last processed block
             last_processed_block = latest_block
-            update_last_processed_block_in_db(last_processed_block)
+            update_last_processed_block_in_db(c, last_processed_block) # pass the cursor to the update function
+            conn.commit()
 
         # Debug 
         except Exception as e:
@@ -143,50 +152,57 @@ async def burn_check():
             print(f'Response text: {await response.text()}')
             print(f'Error: {e}')
 
+    # close the database connection here
+    conn.close()
+
 async def calculate_total_burned_coins():
     global global_total_burned_coins, last_processed_block
 
+    # Open the database connection here
+    conn = sqlite3.connect('burnbot.db')
+    c = conn.cursor()
+
     async with aiohttp.ClientSession(auth=auth) as session:
-        # fetch the latest block
-        async with session.post('http://localhost:14531', json={'method': 'getinfo'}) as response:
-            response_json = await response.json()
-            latest_block = response_json['result']['blocks']
-
-        # we will start from the last processed block + 1 and iterate up to the latest block
-        for current_block in range(last_processed_block + 1, latest_block + 1):
-            print(f'Checking block {current_block}')  # Debug line
-
-            async with session.post('http://localhost:14531', json={'method': 'getblockhash', 'params': [current_block]}) as response:
+        try:
+            async with session.post('http://localhost:14531', json={'method': 'getinfo'}) as response:
                 response_json = await response.json()
-                block_hash = response_json['result']
-                print(f'Block hash: {block_hash}')  # Debug line
+                latest_block = response_json['result']['blocks']
 
-            async with session.post('http://localhost:14531', json={'method': 'getblock', 'params': [block_hash]}) as response:
-                response_json = await response.json()
-                block = response_json['result']
-
-            # iterate through each transaction in the block
-            for txid in block['tx']:
-                async with session.post('http://localhost:14531', json={'method': 'getrawtransaction', 'params': [txid, 1]}) as response:
+            # start from the last processed block
+            for i in range(last_processed_block + 1, latest_block + 1):
+                # calculate total burned coins
+                async with session.post('http://localhost:14531', json={'method': 'getblockhash', 'params': [i]}) as response:
                     response_json = await response.json()
-                    tx = response_json['result']
-                    if tx is None:
-                        print(f'Failed to get raw transaction {txid}') # Debug line
-                        continue  # skip this transaction and move to the next one
+                    block_hash = response_json['result']
 
-                # check each output script for the OP_RETURN opcode
-                for vout in tx['vout']:
-                    if vout['scriptPubKey']['asm'].startswith('OP_RETURN'):
-                        # add the value of this output to the total burned coins
-                        global_total_burned_coins += vout['value']
-                        update_total_burned_coins_in_db(global_total_burned_coins)
+                total_burned_coins_this_block = 0
+                async with session.post('http://localhost:14531', json={'method': 'getblock', 'params': [block_hash]}) as response:
+                    response_json = await response.json()
+                    block = response_json['result']
 
-                        # print out the sync progress and total burned coins so far
-                        print(f'Sync progress: Block {current_block}/{latest_block} checked. Total burned coins so far: {global_total_burned_coins}') # Debug line
+                for txid in block['tx']:
+                    async with session.post('http://localhost:14531', json={'method': 'getrawtransaction', 'params': [txid, 1]}) as response:
+                        response_json = await response.json()
+                        tx = response_json['result']
+                        if tx is None:
+                            continue
 
-            # update last processed block
-            last_processed_block = current_block
-            update_last_processed_block_in_db(last_processed_block)
+                    for vout in tx['vout']:
+                        if vout['scriptPubKey']['asm'].startswith('OP_RETURN'):
+                            total_burned_coins_this_block += vout['value']
+
+                global_total_burned_coins += total_burned_coins_this_block
+
+            update_total_burned_coins_in_db(c, global_total_burned_coins)
+            conn.commit()
+
+        except Exception as e:
+            print(f'Response status: {response.status}')
+            print(f'Response text: {await response.text()}')
+            print(f'Error: {e}')
+
+    # close the database connection here
+    conn.close()
 
 # replace with your actual bot token
 bot.run('your-bot-token')
