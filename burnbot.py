8dfcc7ecc9734fb64d103cca69033128d77e72ac
0xCiruitBreaker <sarris88p@gmail.com>
2023-06-13 16:15:22 -0400

    fix block skipping in burn_check, add debug for discord embed messages

--- burnbot.py
+++ burnbot.py
@@ -81,32 +81,29 @@ async def burn_check():
                 response_json = await response.json()
                 latest_block = response_json['result']['blocks']
 
-            # check if the latest block is the same as the last processed block
-            if latest_block == last_processed_block:
-                # if it is, no need to process it again
-                return
+            # iterate through each unprocessed block
+            for current_block in range(last_processed_block + 1, latest_block + 1):
+                print(f'Checking block {current_block}')  # Debug line
 
-            print(f'Checking block {latest_block}')  # Debug line
-
-            async with session.post('http://localhost:14531', json={'method': 'getblockhash', 'params': [latest_block]}) as response:
-                response_json = await response.json()
-                block_hash = response_json['result']
-                print(f'Block hash: {block_hash}')  # Debug line
-
-            # Burned coins in the current block
-            total_burned_coins_this_block = 0
-            burn_txid_this_block = None
-
-            async with session.post('http://localhost:14531', json={'method': 'getblock', 'params': [block_hash]}) as response:
-                response_json = await response.json()
-                block = response_json['result']
-
-            # iterate through each transaction in the block
-            for txid in block['tx']:
-                async with session.post('http://localhost:14531', json={'method': 'getrawtransaction', 'params': [txid, 1]}) as response:
+                async with session.post('http://localhost:14531', json={'method': 'getblockhash', 'params': [current_block]}) as response:
                     response_json = await response.json()
-                    print(f'Response JSON for raw transaction: {response_json}')  # Debug line
-                    tx = response_json['result']
+                    block_hash = response_json['result']
+                    print(f'Block hash: {block_hash}')  # Debug line
+
+                # Burned coins in the current block
+                total_burned_coins_this_block = 0
+                burn_txid_this_block = None
+
+                async with session.post('http://localhost:14531', json={'method': 'getblock', 'params': [block_hash]}) as response:
+                    response_json = await response.json()
+                    block = response_json['result']
+
+                # iterate through each transaction in the block
+                for txid in block['tx']:
+                    async with session.post('http://localhost:14531', json={'method': 'getrawtransaction', 'params': [txid, 1]}) as response:
+                        response_json = await response.json()
+                        print(f'Response JSON for raw transaction: {response_json}')  # Debug line
+                        tx = response_json['result']
                     if tx is None:
                         print(f'Failed to get raw transaction {txid}.') # Debug line
                         continue  # Skip this transaction and move to the next one
@@ -130,13 +127,19 @@ async def burn_check():
                         print('No channel found with specified ID for channel 1')  # Debug line
                     else:
                         print(f'Sending message to channel {channel_1.id}')  # Debug line
-                        embed=discord.Embed(title="Burn transaction detected!", color=0xff0000)
-                        embed.add_field(name="Block number", value=latest_block, inline=False)
-                        embed.add_field(name="Block hash", value=block_hash, inline=False)
-                        embed.add_field(name="Transaction ID", value=burn_txid_this_block, inline=False)
-                        embed.add_field(name="Burned coins in this block", value=total_burned_coins_this_block, inline=False)
-                        embed.add_field(name="Total burned coins", value=global_total_burned_coins, inline=False)
-                        await channel_1.send(embed=embed)
+
+                        # Step 1: Catch exceptions during message preparation and sending
+                        try:
+                            # Step 2: Validate the embed format. Convert the values to string before passing to the add_field method.
+                            embed=discord.Embed(title="Burn transaction detected!", color=0xff0000)
+                            embed.add_field(name="Block number", value=str(latest_block), inline=False)
+                            embed.add_field(name="Block hash", value=str(block_hash), inline=False)
+                            embed.add_field(name="Transaction ID", value=str(burn_txid_this_block), inline=False)
+                            embed.add_field(name="Burned coins in this block", value=str(total_burned_coins_this_block), inline=False)
+                            embed.add_field(name="Total burned coins", value=str(global_total_burned_coins), inline=False)
+                            await channel_1.send(embed=embed)
+                        except Exception as e:
+                            print(f'Exception occurred while creating/sending the embed: {e}') # Debug line
         
                     if channel_2 is None: # Comment out starting here if you want only one channel
                         print('No channel found with specified ID for channel 2')  # Debug line
@@ -150,14 +153,14 @@ async def burn_check():
                                         f'Total burned coins: {global_total_burned_coins}') # Comment out ending here if you want only one channel
 
             # update last processed block
-            last_processed_block = latest_block
+            last_processed_block = current_block
             update_last_processed_block_in_db(last_processed_block)
 
         # Debug 
         except Exception as e:
             print(f'Response status: {response.status}')
             print(f'Response text: {await response.text()}')
-            print(f'Error: {e}')
+            print(f'Exception occurred while processing blocks: {e}')  # Debug line
 
 async def calculate_total_burned_coins():
     global global_total_burned_coins, last_processed_block
